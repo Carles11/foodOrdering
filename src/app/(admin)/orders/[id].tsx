a7ba94cae452fe5c6a76ab28cb1125502f7cfece
carlos <c.delriofrances@gmail.com>
2024-11-24 00:39:08 +0100

    frontend UI done

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,8 +1,10 @@
 import orders from '@/assets/data/orders'
 import OrderItemListItem from '@/components/OrderItemListItem'
 import OrderListItem from '@/components/OrderListItem'
+import Colors from '@/constants/Colors'
+import { OrderStatusList } from '@/types'
 import { Stack, useLocalSearchParams } from 'expo-router'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 
 const OrderDetailScreen = () => {
   const { id } = useLocalSearchParams()
@@ -17,12 +19,44 @@ const OrderDetailScreen = () => {
     <View style={styles.container}>
       <Stack.Screen options={{ title: `Order #${order.id}` }} />
 
-      <OrderListItem order={order} />
-
       <FlatList
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 10 }}
+        ListHeaderComponent={() => <OrderListItem order={order} />}
+        ListFooterComponent={() => (
+          <>
+            <Text style={{ fontWeight: 'bold' }}>Status</Text>
+            <View style={{ flexDirection: 'row', gap: 5 }}>
+              {OrderStatusList.map((status) => (
+                <Pressable
+                  key={status}
+                  onPress={() => console.warn('Update status')}
+                  style={{
+                    borderColor: Colors.light.tint,
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 5,
+                    marginVertical: 10,
+                    backgroundColor:
+                      order.status === status
+                        ? Colors.light.tint
+                        : 'transparent'
+                  }}
+                >
+                  <Text
+                    style={{
+                      color:
+                        order.status === status ? 'white' : Colors.light.tint
+                    }}
+                  >
+                    {status}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
+          </>
+        )}
       />
     </View>
   )
--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -2,14 +2,11 @@ import { FlatList, Platform, View } from 'react-native'
 
 import orders from '@/assets/data/orders'
 import OrdersListItem from '@/components/OrderListItem'
-import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 
 export default function MenuScreen() {
   return (
     <View>
-      <Stack.Screen options={{ title: 'Orders' }} />
-
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
 
       <FlatList
