76d3713afcf27b50cd389148186eba871f721916
carlos <c.delriofrances@gmail.com>
2024-12-19 15:14:37 +0100

    fixed too many rerenders after logout

--- src/app/(user)/profile.tsx
+++ src/app/(user)/profile.tsx
@@ -1,11 +1,15 @@
 import { supabase } from '@/lib/supabase'
+import { useRouter } from 'expo-router'
 import { Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
 
 export default function ProfileScreen() {
+  const router = useRouter()
+
   const logOut = async () => {
     try {
       await supabase.auth.signOut()
+      router.push('/sign-in')
     } catch (error) {
       console.error('Error signing out:', error)
     }
